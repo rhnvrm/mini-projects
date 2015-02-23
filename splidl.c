2794a19efae558f93c22e5d62635d45ac50b748e
rhnvrm <rohanverma2004@gmail.com>
2015-02-23 19:47:13 +0530

    Added Error output

--- splidl.c
+++ splidl.c
@@ -1,75 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
- 
 #include <curl/curl.h>
- 
-static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+
+size_t write_func(void *ptr, size_t size, size_t nmemb, FILE *stream);
+
+void DownloadURL(char* url, const char* file);
+
+//callback function for libcurl
+size_t write_func(void *ptr, size_t size, size_t nmemb, FILE *stream)
 {
-  int written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+     return fwrite(ptr, size, nmemb, stream);
 }
- 
+
+void DownloadURL(char* url, const char* file){
+    CURL *curl;
+    CURLcode res;
+    FILE *outfile;
+    curl = curl_easy_init();
+    if(curl)
+    {
+        outfile = fopen(file, "w");
+        printf ( "downloading the url %s\n", url );
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, outfile);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_func);
+
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(outfile);
+    }
+}
+
+
+
 int main(void)
 {
-  CURL *curl_handle;
-  static const char *headerfilename = "head.out";
-  FILE *headerfile;
-  static const char *bodyfilename = "body.out";
-  FILE *bodyfile;
- 
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* init the curl session */ 
-  curl_handle = curl_easy_init();
- 
-  /* input the website to save*/
-  char input[255];
-  printf("Enter the website to save: ");
-  scanf("%s", input);
- 
- 
-  /* set URL to get */ 
-  curl_easy_setopt(curl_handle, CURLOPT_URL, input);
- 
-  /* no progress meter please */ 
-  //curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
- 
-  /* send all data to this function  */ 
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
- 
-  /* open the header file */ 
-  headerfile = fopen(headerfilename, "wb");
-  if(!headerfile) {
-    curl_easy_cleanup(curl_handle);
-    return -1;
-  }
- 
-  /* open the body file */ 
-  bodyfile = fopen(bodyfilename, "wb");
-  if(!bodyfile) {
-    curl_easy_cleanup(curl_handle);
-    fclose(headerfile);
-    return -1;
-  }
- 
-  /* we want the headers be written to this file handle */ 
-  curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, headerfile);
- 
-  /* we want the body be written to this file handle instead of stdout */ 
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, bodyfile);
- 
-  /* get it! */ 
-  curl_easy_perform(curl_handle);
- 
-  /* close the header file */ 
-  fclose(headerfile);
- 
-  /* close the body file */ 
-  fclose(bodyfile);
- 
-  /* cleanup curl stuff */ 
-  curl_easy_cleanup(curl_handle);
- 
-  return 0;
+    char *url = "https://github.com/rhnvrm/splidl/archive/master.zip";
+    DownloadURL(url, "master.zip");
+
 }
