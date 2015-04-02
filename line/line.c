8f9b7151e03039764b245ba05cae570f05be212e
rhnvrm <rohanverma2004@gmail.com>
2015-04-03 02:17:20 +0530

    setup some basic speed control

--- line/line.c
+++ line/line.c
@@ -2,13 +2,16 @@
 #include <avr/io.h>       // include avr header file
 #include <util/delay.h>   // include delay header file
 
+// use _delay_ms(ms) for delaying
 
 int main(void)  		  // main function
 {
 DDRA=0b00000000;          // declare PortA as a input port connected to the sensors
-DDRC=0b11111111;          // declare PortB as a output port connected to the motors
+DDRC=0b11111111;          // declare PortC as a output port connected to the motors
 
 int ls=0, rs=0;
+int flag = 1;
+int speed = 1;
 
  
 while(1) 				  // infinite loop
@@ -17,15 +20,23 @@ while(1) 				  // infinite loop
 	rs=PINA&0b00100000;   // mask PA5 bit of Port 
 
 	 
-	 
 	if((ls==0b00000000) && (rs==0b00000000))      // if both sensors "off"
 		{
 		PORTC=0b00000000;  // stop	
+
 		}
 
     if((ls==0b00010000) && (rs==0b00100000))      // if both sensors "on"
 		{
-        PORTC=0b00001001;  // move straight
+        	if(flag >= speed){
+			flag = 0;
+			PORTC=0b00001001;  // move straight
+		}
+		else
+			PORTC=0b00000000;  // stop	
+
+		flag++;
+
 		}
 			
 	if((ls==0b00000000) && (rs==0b00100000)) 	 // if left sensor off but right sensor on
@@ -40,5 +51,5 @@ while(1) 				  // infinite loop
 
 
 	}      				   // while closed
-
+	
 }         				   // main closed-
