73df3ab2035b9aefa729e42c0c1b423de46f1016
rhnvrm <rohanverma2004@gmail.com>
2015-04-16 00:23:24 +0530

    turning improved

--- line/line.c
+++ line/line.c
@@ -2,6 +2,8 @@
 #include <avr/io.h>       // include avr header file
 #include <util/delay.h>   // include delay header file
 
+#define SPEED 3
+
 // use _delay_ms(ms) for delaying
 
 int main(void)  		  // main function
@@ -10,8 +12,8 @@ DDRA=0b00000000;          // declare PortA as a input port connected to the sens
 DDRC=0b11111111;          // declare PortC as a output port connected to the motors
 
 int ls=0, rs=0;
-int flag = 1;
-int speed = 1;
+int flag = 1, slow = 0;
+int speed = SPEED;
 
  
 while(1) 				  // infinite loop
@@ -22,18 +24,24 @@ while(1) 				  // infinite loop
 	 
 	if((ls==0b00000000) && (rs==0b00000000))      // if both sensors "off"
 		{
-		PORTC=0b00000000;  // stop	
+			PORTC=0b00000000;  // stop	
 
 		}
 
     if((ls==0b00010000) && (rs==0b00100000))      // if both sensors "on"
 		{
         	if(flag >= speed){
-			flag = 0;
-			PORTC=0b00001001;  // move straight
-		}
-		else
-			PORTC=0b00000000;  // stop	
+				flag=0;
+				PORTC=0b00001001;
+				if(slow == 0) speed = SPEED;
+				else{
+					speed = SPEED + 3;
+					slow--;
+				}
+			}  // move straight
+		
+			else
+				PORTC=0b00000000;  // stop	
 
 		flag++;
 
@@ -41,12 +49,18 @@ while(1) 				  // infinite loop
 			
 	if((ls==0b00000000) && (rs==0b00100000)) 	 // if left sensor off but right sensor on
         {
-		PORTC=0b00010001;  // turn left
+			PORTC=0b00010001;  // turn left
+			//flag = -5;
+			slow = 1;
+			//speed = SPEED + 3;
         }
 
 	if((ls==0b00010000)&&(rs==0b00000000))  	 	 // if left sensor on but right sensor off
         {
-		PORTC=0b00011000;  // turn right
+			PORTC=0b00011000;  // turn right
+			//flag = -5;
+			slow = 1;
+			//speed = SPEED + 3;
 		}
 
 
