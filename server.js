42f3de14c70911568c34f6277c93df877419acbc
rhnvrm <rohanverma2004@gmail.com>
2015-08-31 05:04:20 +0530

    fix server

--- server.js
+++ server.js
@@ -1,131 +1,66 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');    // call express
-var app        = express();         // define our app using express
+var express = require('express');
 var bodyParser = require('body-parser');
-var Bear     = require('./app/models/bear');
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/animals'); // connect to our database
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var restify = require('express-restify-mongoose')
+ 
+mongoose.connect('mongodb://localhost/database');
+ 
+var MarkerSchema = new Schema({
+  location: { 
+    type: [Number],  // [<longitude>, <latitude>]
+    index: '2d'
+  },
+  formatted_address: {
+    type: String,
+    index: 'text'
+  },
+  gender: Number,
+  rooms: {
+    max_people: Number,
+    rent: Number,
+  security: Number
+  },
+  galleryurl: String,
+  services: {
+    food: {
+      breakfast: Number,
+      lunch: Number,
+      dinner: Number
+    },
+    housekeeping: {
+      remark: String,
+      cost: Number
+    },
+    parking: String,
+    laundry: String
+  },
+  rules:{
+      drinking: Number,
+      smoking: Number
+  }
+});
+MarkerSchema.index({location: '2d', formatted_address: "text"});
+var MarkerModel = mongoose.model('Marker', MarkerSchema);
+ /*
+var Invoice = new Schema({
+    customer: { type: Schema.Types.ObjectId, ref: 'Customer' },
+    amount: { type: Number, required: true }
+});
+var InvoiceModel = mongoose.model('Invoice', Invoice);
+ */
 
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+var app = express();
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;    // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-
-var router = express.Router();        // get an instance of the express Router
-// middleware to use for all requests
-router.use(function(req, res, next) {
-  // do logging
-  console.log('Something is happening.');
-  next(); // make sure we go to the next routes and don't stop here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+ 
+var router = express.Router();
+restify.serve(router, MarkerModel);
+//restify.serve(router, InvoiceModel);
+app.use(router);
+ 
+app.listen(8080, function() {
+    console.log("Express server listening on port 8080");
 });
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// more routes for our API will happen here
-
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
-
-  // create a bear (accessed at POST http://localhost:8080/api/bears)
-  .post(function(req, res) {
-    var bear = new Bear();    // create a new instance of the Bear model
-    bear.name = req.body.name;  // set the bears name (comes from the request)
-    bear.type = req.body.type;
-    bear.color = req.body.color;
-    bear.age = req.body.age;
-    bear.ferocity = req.body.ferocity;
-    console.log(bear, bear.name, req.body.name);
-    // save the bear and check for errors
-    bear.save(function(err) {
-      console.log('bear.save');
-      if (err) {
-        res.send(err);
-      } else {
-        res.json({ message: 'Bear created!' });
-      }
-    });
-  })
-
-  // get all the bears (accessed at GET http://localhost:8080/api/bears)
-  .get(function(req, res) {
-    Bear.find(function(err, bears) {
-      if (err)
-        res.send(err);
-
-      res.json(bears);
-    });
-  });
-
-  router.route('/bears/:bear_id')
-
-  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-  .get(function(req, res) {
-    Bear.findById(req.params.bear_id, function(err, bear) {
-      if (err)
-        res.send(err);
-      res.json(bear);
-    });
-  })
-
-  // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-  .put(function(req, res) {
-
-    // use our bear model to find the bear we want
-    Bear.findById(req.params.bear_id, function(err, bear) {
-
-      if (err)
-        res.send(err);
-
-      bear.name = req.body.name;  // update the bears info
-      bear.type = req.body.type;
-      bear.color = req.body.color;
-      bear.age = req.body.age;
-      bear.ferocity = req.body.ferocity;
-
-      // save the bear
-      bear.save(function(err) {
-        if (err)
-          res.send(err);
-
-        res.json({ message: 'Bear updated!' });
-      });
-
-    });
-  })
-
-  // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-  .delete(function(req, res) {
-    Bear.remove({
-      _id: req.params.bear_id
-    }, function(err, bear) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Successfully deleted' });
-    });
-  });
-
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
