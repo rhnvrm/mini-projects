9c201db6abd8a20307bed6d751367a5266400186
rhnvrm <rohanverma2004@gmail.com>
2015-09-07 13:52:06 +0530

    better test

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ var restify = require('express-restify-mongoose');
 var cors = require('cors');
 
  
-mongoose.connect('mongodb://user:passw0rd@waffle.modulusmongo.net:27017/abeqoS9i');
+mongoose.connect('mongodb://localhost/database');
  
 var MarkerSchema = new Schema({
-  location: { 
-    type: [Number],  // [<longitude>, <latitude>]
-    index: '2d'
-  },
+  loc: [],
   formatted_address: {
     type: String,
     index: 'text'
@@ -43,8 +40,8 @@ var MarkerSchema = new Schema({
       smoking: Number
   }
 });
-MarkerSchema.index({location: '2d', formatted_address: "text"});
-var MarkerModel = mongoose.model('Marker', MarkerSchema);
+MarkerSchema.index({ loc: '2d' });
+var Marker = mongoose.model('Marker', MarkerSchema);
 
 
 var app = express();
@@ -55,10 +52,24 @@ app.use(cors());
 
  
 var router = express.Router();
-restify.serve(router, MarkerModel);
+restify.serve(router, Marker);
 //restify.serve(router, InvoiceModel);
 app.use(router);
  
 app.listen(8080, function() {
-    console.log("Express server listening on port 8080");
+    console.log("Express server listening on port 8080");/*
+    Marker.find({ loc : { '$near' : [78.881213, 28.366455] } }, console.log);
+    Marker.find({ loc : { '$near' : [78.881213, 28.366455] } }).limit(5).exec(console.log);*/
+     
+
+     /*mongoose.connection.db.executeDbCommand({ 
+  geoNear : 'Marker',  // the mongo collection
+  near : [78.881213, 28.366455], // the geo point
+  spherical : true,  // tell mongo the earth is round, so it calculates based on a 
+                     // spherical location system
+  distanceMultiplier: 6371, // tell mongo how many radians go into one kilometer.
+  maxDistance : 1/6371, // tell mongo the max distance in radians to filter out
+}, function(err, result) {
+  console.log(result.documents[0].results);
+}); */
 });
