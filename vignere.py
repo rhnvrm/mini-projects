1d2c836701360f096be7a79a41daf2b4816d9edc
rhnvrm <rohanverma2004@gmail.com>
2015-10-23 21:36:14 +0530

    completed vignere

--- vignere.py
+++ vignere.py
@@ -1,10 +1,25 @@
-ciphertext = 'abc'
-vignere = 'key'
+ciphertext = 'commonsenseisnotsocommon'
+key = 'pizza'
 
 def itoc(x):
-	return chr(x+ ord('a'))
+	return chr(x +  ord('a')) 
 
 def ctoi(x):
-	return ord(x) - ord('a')
+	return (ord(x) - ord('a')) % 26
 
 print ctoi('a'), ctoi('z')
+print itoc(0), itoc(25)
+
+
+while(len(key) < len(ciphertext)):
+	key = key + key
+
+print "+ve: "
+for i in xrange(0, len(ciphertext)):
+	print itoc((ctoi(ciphertext[i]) - ctoi(key[i]))%26) , 
+print ""
+
+print "-ve: "
+for i in xrange(0, len(ciphertext)):
+	print itoc((ctoi(ciphertext[i]) - ctoi(key[i]))%26) , 
+print ""
