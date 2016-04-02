800f36a1da40f0a2d4dd003a403a995669674e10
rhnvrm <rohanverma2004@gmail.com>
2016-04-02 22:01:34 +0530

    fixed for different repos

--- github-search-api/github.py
+++ github-search-api/github.py
@@ -5,7 +5,7 @@ import json
 
 #atom atom 27-02-2013
 
-L = input().split()
+L = input().split(' ')
 
 org = L[0]
 repo = L[1]
@@ -15,8 +15,12 @@ date_str = '' + date[2] + '-' + date[1] + '-' + date[0]
 
 #print(org, repo, date_str)
 
+
 r = requests.get('https://api.github.com/search/issues?q=user:'+org+'+repo:'+repo+'+created:'+date_str+'&sort=created&order=asc').json()
 
+#print(r['items'][0])
 
 for i in range(0, len(r['items'])):
-	print(r['items'][i]['html_url'])
+	gen_url = "https://github.com/"+org+"/"+repo
+	if(gen_url == r['items'][i]['html_url'][:len(gen_url)]):
+		print(r['items'][i]['html_url'])
